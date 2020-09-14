1a15ba63a43742229571b7ff1ad3fafff6f3b2eb
JessPinkman <serge.goncalves@gmail.com>
2020-09-14 08:32:48 +0800

    changed Pure static method names

--- src/Pure.php
+++ src/Pure.php
@@ -13,14 +13,14 @@ class Pure
         return $component;
     }
 
-    public static function buffer(callable $func, ...$args): string
+    public static function pureBuffer(callable $func, ...$args): string
     {
         ob_start();
         \call_user_func($func, ...$args);
         return \ob_get_clean();
     }
 
-    public static function fromTag(string $tag, ?array $children = null): Component
+    public static function pureFrom(string $tag, ?array $children = null): Component
     {
         $component = new Component($tag);
         if (count($children)) {
@@ -29,17 +29,17 @@ class Pure
         return $component;
     }
 
-    public static function openTag($tag): string
+    public static function pureOpen($tag): string
     {
         return "<$tag>";
     }
 
-    public static function closeTag($tag): string
+    public static function pureClose($tag): string
     {
         return "</$tag>";
     }
 
-    public static function addComment($comment): string
+    public static function pureComment($comment): string
     {
         return "<!-- $comment -->";
     }
