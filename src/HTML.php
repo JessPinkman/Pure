47721e34027f5a107176465aae9df1b1b50c9fe9
JessPinkman <serge.goncalves@gmail.com>
2021-01-28 21:15:41 +0800

    bug fix html

--- src/HTML.php
+++ src/HTML.php
@@ -4,6 +4,12 @@ namespace Pure;
 
 class HTML extends Component
 {
+
+    public function __construct()
+    {
+        parent::__construct('html');
+    }
+
     public function __toString(): string
     {
         return '<!DOCTYPE html>' . parent::__toString();
