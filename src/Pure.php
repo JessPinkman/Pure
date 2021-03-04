02c4ad80a8a2012307e805946853194f5efa8f5b
jesspinkman <serge.goncalves@gmail.com>
2021-03-04 17:33:41 +0800

    added phpdoc method

--- src/Pure.php
+++ src/Pure.php
@@ -2,13 +2,31 @@
 
 namespace Pure;
 
+/**
+ * @method static Component div
+ * @method static Component p
+ * @method static Component span
+ * @method static Component ul
+ * @method static Component ol
+ * @method static Component li
+ * @method static Component h1
+ * @method static Component h2
+ * @method static Component h3
+ * @method static Component h4
+ * @method static Component h5
+ * @method static Component h6
+ * @method static Component head
+ * @method static Component title
+ * @method static Component meta
+ * @method static Component body
+ */
 class Pure
 {
     public static function __callStatic(string $tag, array $children): Component
     {
         $component = new Component($tag);
         if (count($children)) {
-            $component->append($children);
+            $component->___($children);
         }
         return $component;
     }
@@ -24,7 +42,7 @@ class Pure
     {
         $component = new Component($tag);
         if (count($children)) {
-            $component->append($children);
+            $component->___($children);
         }
         return $component;
     }
